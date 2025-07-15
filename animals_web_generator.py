969a352089bdb3e230d88ac59e9f751947bbd442
AI-Engineer2025 <AI-Engineer2025@github.com>
2025-07-15 21:50:42 +0200

    Ausgabe aus der json funktioniert

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,18 @@
+import json
 
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+
+animals_data = load_data('animals_data.json')
+
+
+
+for animal in animals_data:
+  print("Name:",animal["name"],"\n"
+        "Diet:",animal["characteristics"]["diet"],"\n"
+        "Location:",animal["locations"][0],"\n"
+        "Type:",animal["taxonomy"]["class"],
+        "\n")
