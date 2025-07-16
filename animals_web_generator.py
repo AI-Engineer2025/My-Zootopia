feede6b6dfe25af5ede2fa376da03171bed97436
AI-Engineer2025 <AI-Engineer2025@github.com>
2025-07-16 17:09:19 +0200

    verbessere HTML der Karten-Elemente

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,14 +1,13 @@
 import json
 
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
 
 animals_data = load_data('animals_data.json')
-
-
 
 """for animal in animals_data:
   print("Name:",animal["name"],"\n"
@@ -17,16 +16,16 @@ animals_data = load_data('animals_data.json')
         "Type:",animal["taxonomy"]["class"],
         "\n")"""
 
-
 output = ""  # define an empty string
 for animal in animals_data:
-    # append information to each string
-    output += '<li class="cards__item"><br/>\n'
-    output += f"Name: {animal['name']}<br/>\n"
-    output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-    output += f"Location: {animal["locations"][0]}<br/>\n"
-    output += f"Type: {animal["taxonomy"]["class"]}<br/>\n"
-    output += "</li>"
+    output += '<li class="cards__item">\n'
+    output += f'  <div class="card__title">{animal["name"]}</div>\n'
+    output += '  <p class="card__text">\n'
+    output += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+    output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+    output += f'      <strong>Type:</strong> {animal["taxonomy"]["class"]}<br/>\n'
+    output += '  </p>\n'
+    output += '</li>\n'
 print(output)
 
 with open("animals_template.html", "r", encoding="utf-8") as datei:
@@ -36,5 +35,3 @@ inhalt = inhalt.replace("__REPLACE_ANIMALS_INFO__", output)
 
 with open("animals.html", "w", encoding="utf-8") as datei:
     datei.write(inhalt)
-
-#print(inhalt)
