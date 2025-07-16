10f3b3edeb7725da9878dc52a4aa7fb980f4c8a8
AI-Engineer2025 <AI-Engineer2025@github.com>
2025-07-16 16:24:36 +0200

    Tierdaten in HTML einfügen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,9 +10,30 @@ animals_data = load_data('animals_data.json')
 
 
 
-for animal in animals_data:
+"""for animal in animals_data:
   print("Name:",animal["name"],"\n"
         "Diet:",animal["characteristics"]["diet"],"\n"
         "Location:",animal["locations"][0],"\n"
         "Type:",animal["taxonomy"]["class"],
-        "\n")
+        "\n")"""
+
+
+output = ""  # define an empty string
+for animal in animals_data:
+    # append information to each string
+    output += f"Name: {animal['name']}\n"
+    output += f"Diet: {animal['characteristics']['diet']}\n"
+    output += f"Location: {animal["locations"][0]}\n"
+    output += f"Type: {animal["taxonomy"]["class"]}\n"
+
+#print(output)
+
+with open("animals_template.html", "r", encoding="utf-8") as datei:
+    inhalt = datei.read()
+
+inhalt = inhalt.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w", encoding="utf-8") as datei:
+    datei.write(inhalt)
+
+print(inhalt)
